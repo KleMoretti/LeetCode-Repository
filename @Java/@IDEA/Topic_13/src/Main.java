c284ac72a2dfb3bbcf4ac4f5fad79b9cc55b4215
KleMoretti <niuniuniudechao@outlook.com>
2025-03-04 20:30:26 +0800

    Java in IDEA

---
+++ @Java/@IDEA/Topic_13/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    private int getValue(char ch){
+        switch(ch){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public int romanToInt(String s){
+        int result=0;
+        int prev=getValue(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            int curr=getValue(s.charAt(i));
+            if(prev<curr){
+                result-=prev;
+            }else{
+                result+=prev;
+            }
+            prev=curr;
+        }
+        return result+prev;
+    }
+}
