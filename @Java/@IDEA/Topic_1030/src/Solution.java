8487355df0276e66165c8e19078a49b42db23e75
KleMoretti <niuniuniudechao@outlook.com>
2025-03-10 17:36:34 +0800

    Update 1030
    用List，想到了不会用，直接法，sort方法

--- @Java/@IDEA/Topic_1030/src/Solution.java
+++ @Java/@IDEA/Topic_1030/src/Solution.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
@@ -20,11 +23,42 @@ class Solution {
         return res;
     }
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int[][] res = s.allCellsDistOrder(3, 5, 2, 3);
-        for (int[] re : res) {
-            System.out.println(Arrays.toString(re));
+    public int[][] allCellsDistOrder2(int rows, int cols, int rCenter, int cCenter) {
+        int[][] ret = new int[rows * cols][];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret[i * cols + j] = new int[]{i, j};
+            }
         }
+        Arrays.sort(ret, (a, b) ->
+                (Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)) - (Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter)));
+        return ret;
+    }
+
+    public int[][] allCellsDistOrder3(int rows, int cols, int rCenter, int cCenter) {
+        int maxDist = Math.max(rCenter, rows - 1 - rCenter) + Math.max(cCenter, cols - 1 - cCenter);
+        List<List<int[]>> bucket = new ArrayList<List<int[]>>();
+        for (int i = 0; i <= maxDist; i++) {
+            bucket.add(new ArrayList<int[]>());
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int d = dist(i, j, rCenter, cCenter);
+                bucket.get(d).add(new int[]{i, j});
+            }
+        }
+        int[][] ret = new int[rows * cols][];
+        int index = 0;
+        for (int i = 0; i <= maxDist; i++) {
+            for (int[] it : bucket.get(i)) {
+                ret[index++] = it;
+            }
+        }
+        return ret;
+    }
+
+    public int dist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
     }
 }
